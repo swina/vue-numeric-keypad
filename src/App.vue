e509341ec7a58f0e750eec35229121184e2aec17
Antonio <swina.allen@gmail.com>
2019-01-21 23:12:57 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Number is : {{ number }}<br />
+    <a href="#" @click="showKeypad = !showKeypad;"><h4>Toggle Keypad</h4></a>
+    <numericpad
+      :onInput="onInput"
+      :onDelete="onDelete"
+      :onReset="onReset"
+      :show="showKeypad"
+    />
+  </div>
+</template>
+<script>
+import numericpad from "@/components/numeric-keypad.vue";
+
+export default {
+  name: "app-numeric-keypad",
+  components: {
+    numericpad
+  },
+  data: () => ({
+    number: "",
+    maxLength: 6,
+    showKeypad: true
+  }),
+  methods: {
+    onInput(key) {
+      this.number = (this.number + key).slice(0, this.maxLength);
+    },
+    onDelete() {
+      this.number = this.number.slice(0, this.number.length - 1);
+    },
+    onReset() {
+      this.number = "";
+    }
+  }
+};
+</script>
